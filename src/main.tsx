e4f65bc170569b554452329c4387788b853a5e24
Jabs69 <jbenitezsaez@gmail.com>
2025-07-21 01:21:34 -0400

    corrigiendo basename en el enrutador

--- src/main.tsx
+++ src/main.tsx
@@ -1,39 +1,44 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router';
-import App from './App';
-import Layout from './layouts/dashboard';
-import DashboardPage from './pages';
-import OrdersPage from './pages/orders';
-import SignInPage from './pages/signIn';
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import App from "./App";
+import Layout from "./layouts/dashboard";
+import DashboardPage from "./pages";
+import OrdersPage from "./pages/orders";
+import SignInPage from "./pages/signIn";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      Component: App,
+      children: [
+        {
+          path: "/",
+          Component: Layout,
+          children: [
+            {
+              path: "/",
+              Component: DashboardPage,
+            },
+            {
+              path: "/orders",
+              Component: OrdersPage,
+            },
+          ],
+        },
+        {
+          path: "/sign-in",
+          Component: SignInPage,
+        },
+      ],
+    },
+  ],
   {
-    Component: App,
-    children: [
-      {
-        path: '/',
-        Component: Layout,
-        children: [
-          {
-            path: '/',
-            Component: DashboardPage,
-          },
-          {
-            path: '/orders',
-            Component: OrdersPage,
-          },
-        ],
-      },
-      {
-        path: '/sign-in',
-        Component: SignInPage,
-      },
-    ],
+    basename: "/inventory-system/",
   },
-]);
+);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>,
